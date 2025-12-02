1909e659df74888461bf458161e97a18a7e50fe3
KallXfalcon <bskranggara@gmail.com>
2025-12-02 18:22:32 +0700

    Initial commit

--- test/main.cpp
+++ test/main.cpp
@@ -3,11 +3,14 @@
 #include <iostream>
 
 int main(){
-    Tensor<vectra::float32> aten = full({5, 5}, 1.0f);
-    Tensor<vectra::float32> bten = flatten(aten);
+
+    Tensor<vectra::float32> aten = randn<vectra::float32>({3, 3});
+    Tensor<vectra::float32> bten = randn<vectra::float32>({3, 3});
+    Tensor<vectra::float32> result = add(aten, bten);
 
     std::cout << aten;
     std::cout << bten;
+    std::cout << result;
 
     return 0;
 }

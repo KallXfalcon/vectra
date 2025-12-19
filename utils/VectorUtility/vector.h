59a22734a4ea770f1a73b09c2a654bbf2f4afb72
KallXfalcon <bskranggara@gmail.com>
2025-12-19 07:21:18 +0700

    v1.1 update

--- utils/VectorUtility/vector.h
+++ utils/VectorUtility/vector.h
@@ -1,7 +1,9 @@
 
 /*
  *
- * vector.h --- v1.0
+ * vector.h (v1.1)
+ * 
+ * v1.1 updated : Safe malloc update
  * 
  * Copyright(C) 2025 KallXfalcon
  * GitHub : https://github.com/KallXfalcon
@@ -26,11 +28,8 @@
 
 #ifdef __cplusplus
 
-#include <immintrin.h>
-#include <type_traits>
 #include <cstdlib>
 #include <cstdio>
-#include <new>
 
 namespace utils {
 
@@ -51,37 +50,12 @@ public:
             return;
         }
 
-        size_t alignment = 1;
-
-        if constexpr (std::is_same_v<T, char> || std::is_same_v<T, int8_t>) alignment = 1;
-        else if constexpr (std::is_same_v<T, short> || std::is_same_v<T, int16_t>) alignment = 2;
-        else if constexpr (std::is_same_v<T, int> || std::is_same_v<T, float> || std::is_same_v<T, int32_t>) alignment = 4;
-        else if constexpr (std::is_same_v<T, double>) alignment = 8;
-        else if constexpr (std::is_same_v<T, __m128i> || std::is_same_v<T, __m128h> || std::is_same_v<T, __m128>  || std::is_same_v<T, __m128d>) alignment = 16;
-        else if constexpr (std::is_same_v<T, __m256i> || std::is_same_v<T, __m256h> || std::is_same_v<T, __m256>  || std::is_same_v<T, __m256d>) alignment = 32;
-        else if constexpr (std::is_same_v<T, __m512i> || std::is_same_v<T, __m512h> || std::is_same_v<T, __m512>  || std::is_same_v<T, __m512d>) alignment = 64;
-        else if constexpr (std::is_same_v<T, size_t>) alignment = sizeof(size_t);
-        else {
-            fprintf(stderr, "Error: Unknown vector type.\n");
-            exit(EXIT_FAILURE);
-        }
-
         const size_t bytes = size_init * sizeof(T);
-        const size_t aligned_bytes = ((bytes + alignment - 1) / alignment) * alignment;
-
-    #ifdef _POSIX_VERSION
-        if (posix_memalign(reinterpret_cast<void**>(&data), alignment, aligned_bytes) != 0) {
-            fprintf(stderr, "Failed to allocate aligned memory!\n");
-            exit(EXIT_FAILURE);
-        }
-    #else
-        data = static_cast<T*>(aligned_alloc(alignment, aligned_bytes));
+        data = static_cast<T*>(malloc(bytes));
         if (!data) {
-            fprintf(stderr, "Failed to allocate aligned memory!\n");
+            fprintf(stderr, "Failed to allocate memory!\n");
             exit(EXIT_FAILURE);
         }
-    #endif
-
         size_ = size_init;
     }
 
@@ -99,33 +73,12 @@ public:
             return;
         }
 
-        size_t alignment = 1;
-
-        if constexpr (std::is_same_v<T, char> || std::is_same_v<T, int8_t>) alignment = 1;
-        else if constexpr (std::is_same_v<T, short> || std::is_same_v<T, int16_t>) alignment = 2;
-        else if constexpr (std::is_same_v<T, int> || std::is_same_v<T, float> || std::is_same_v<T, int32_t>) alignment = 4;
-        else if constexpr (std::is_same_v<T, double>) alignment = 8;
-        else if constexpr (std::is_same_v<T, __m128i> || std::is_same_v<T, __m128h> || std::is_same_v<T, __m128>  || std::is_same_v<T, __m128d>) alignment = 16;
-        else if constexpr (std::is_same_v<T, __m256i> || std::is_same_v<T, __m256h> || std::is_same_v<T, __m256>  || std::is_same_v<T, __m256d>) alignment = 32;
-        else if constexpr (std::is_same_v<T, __m512i> || std::is_same_v<T, __m512h> || std::is_same_v<T, __m512>  || std::is_same_v<T, __m512d>) alignment = 64;
-        else if constexpr (std::is_same_v<T, size_t>) alignment = sizeof(size_t);
-
-        size_t bytes = new_size * sizeof(T);
-        size_t aligned_bytes = ((bytes + alignment - 1) / alignment) * alignment;
-
-    #ifdef _POSIX_VERSION
-        if (posix_memalign(reinterpret_cast<void**>(&data), alignment, aligned_bytes) != 0) {
-            fprintf(stderr, "Failed to allocate aligned memory!\n");
-            exit(EXIT_FAILURE);
-        }
-    #else
-        data = static_cast<T*>(aligned_alloc(alignment, aligned_bytes));
+        const size_t bytes = new_size * sizeof(T);
+        data = static_cast<T*>(malloc(bytes));
         if (!data) {
-            fprintf(stderr, "Failed to allocate aligned memory!\n");
+            fprintf(stderr, "Failed to allocate memory!\n");
             exit(EXIT_FAILURE);
         }
-    #endif
-
         size_ = new_size;
     }
 
@@ -138,6 +91,7 @@ public:
     }
 
     vector(const vector&) = delete;
+
     vector& operator=(const vector&) = delete;
 
     vector(vector&& other) noexcept
@@ -162,6 +116,20 @@ public:
     }
 
     size_t size() const { return size_; }
+
+    void fill(const T& value) {
+        for (size_t i = 0; i < size_; ++i) {
+            data[i] = value;
+        }
+    }
+
+    T& operator[](size_t index) {
+        return data[index];
+    }
+
+    const T& operator[](size_t index) const {
+        return data[index];
+    }
 };
 
 } // namespace utils
